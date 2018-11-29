bfd21aebfe63c444c43f40a655fff1f2169a0b59
Mike Trinkala <trink@acm.org>
2018-11-29 07:11:34 -0800

    Use the Cargo settings for the CLI usage message

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,10 @@ mod parquet {
 }
 
 fn main() {
-    let matches = App::new("parquetfmt")
-        .version("0.1.0")
-        .author("Mike Trinkala <trink@acm.org>")
-        .about("Parquet Schema Formatter")
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(
             Arg::with_name("schema")
                 .required(true)
